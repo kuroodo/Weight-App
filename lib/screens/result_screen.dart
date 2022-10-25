dd7628c05e543a365a326cb9504293a7a246ac4e
Kuroodo <kuroododitory@gmail.com>
2022-10-25 15:59:30 -0400

    Refactor ResultChart in order to have better control of sizing and constraints

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,4 +1,3 @@
-import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:weight_app/helpers/constants.dart';
@@ -88,95 +87,149 @@ class ResultChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DataTable2(
-      columns: [
-        const DataColumn(
-          label: Center(child: Text("Week", textAlign: TextAlign.center)),
-        ),
-        const DataColumn(
-          label: Center(child: Text("Date", textAlign: TextAlign.center)),
-        ),
-        DataColumn(
-          label: Center(
-              child: Text("Weight (${weightMeasurement.weightText})",
-                  textAlign: TextAlign.center)),
-        ),
-        const DataColumn(
-          label:
-              Center(child: Text("Calories Used", textAlign: TextAlign.center)),
-        ),
-        const DataColumn(
-          label: Center(child: Text("Deficit", textAlign: TextAlign.center)),
-        )
-      ],
-      headingTextStyle: const TextStyle(fontSize: 13),
-      dataTextStyle: const TextStyle(fontSize: 15),
-      columnSpacing: 0,
-      horizontalMargin: 0,
-      dataRowHeight: 62,
-      rows: List<DataRow>.generate(
-        data.length,
-        (index) {
-          double weight = weightMeasurement == Measurement.imperial
-              ? Converter.kgToPound(data[index].weight)
-              : data[index].weight;
-          String date = DateFormat("MM/dd/yyyy").format(data[index].date);
-
-          return DataRow(
-              color: MaterialStateProperty.resolveWith<Color?>(
-                  (Set<MaterialState> states) {
-                // All rows will have the same selected color.
-                if (states.contains(MaterialState.selected)) {
-                  return Theme.of(context)
-                      .colorScheme
-                      .primary
-                      .withOpacity(0.08);
-                }
-                // Even rows will have a grey color.
-                if (index.isEven) {
-                  return Colors.grey.withOpacity(0.3);
-                }
-                return null; // Use default value for other states and odd rows.
-              }),
-              cells: [
-                DataCell(
-                  Center(
-                    child: Text("${data[index].week}",
-                        textAlign: TextAlign.center),
-                  ),
-                ),
-                DataCell(
-                  Center(
-                    child: Text(date, textAlign: TextAlign.center),
-                  ),
-                ),
-                DataCell(
-                  Center(
-                    child: Text(
-                      weight.toStringAsFixed(2),
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.yellow),
+    const TextStyle headerStyle = TextStyle(fontSize: 13);
+    const TextStyle dataStyle = TextStyle(fontSize: 15);
+    const double rowHeight = 62;
+    return Column(
+      children: [
+        SizedBox(
+          height: rowHeight,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Row(
+                  children: const [
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        "Week",
+                        textAlign: TextAlign.center,
+                        style: headerStyle,
+                      ),
                     ),
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        "Date",
+                        textAlign: TextAlign.center,
+                        style: headerStyle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        "Weight (${weightMeasurement.weightText})",
+                        textAlign: TextAlign.center,
+                        style: headerStyle,
+                      ),
+                    ),
+                    const Expanded(
+                      flex: 1,
+                      child: Text(
+                        "Calories Used",
+                        textAlign: TextAlign.center,
+                        style: headerStyle,
+                      ),
+                    ),
+                    const Expanded(
+                      flex: 1,
+                      child: Text(
+                        "Deficit",
+                        textAlign: TextAlign.center,
+                        style: headerStyle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              double weight = weightMeasurement == Measurement.imperial
+                  ? Converter.kgToPound(data[index].weight)
+                  : data[index].weight;
+              String date = DateFormat("MM/dd/yyyy").format(data[index].date);
+
+              return Container(
+                color: index.isEven ? Colors.grey.withOpacity(0.3) : null,
+                child: SizedBox(
+                  height: rowHeight,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "${data[index].week}",
+                                textAlign: TextAlign.center,
+                                style: dataStyle,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                date,
+                                textAlign: TextAlign.center,
+                                style: dataStyle,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                          flex: 3,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  weight.toStringAsFixed(2),
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.yellow),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  data[index].calsUsed.toStringAsFixed(2),
+                                  textAlign: TextAlign.center,
+                                  style: dataStyle,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  data[index].deficit.toStringAsFixed(2),
+                                  textAlign: TextAlign.center,
+                                  style: dataStyle,
+                                ),
+                              )
+                            ],
+                          ))
+                    ],
                   ),
                 ),
-                DataCell(
-                  Center(
-                    child: Text(data[index].calsUsed.toStringAsFixed(2),
-                        textAlign: TextAlign.center),
-                  ),
-                ),
-                DataCell(
-                  Center(
-                    child: Text(data[index].deficit.toStringAsFixed(2),
-                        textAlign: TextAlign.center),
-                  ),
-                ),
-              ]);
-        },
-      ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
